76ecd07bac4241fce96c82cf02e674385a97afee
PARKKYEONGCHOON <98440576+PARKKYEONGCHOON@users.noreply.github.com>
2023-01-15 19:11:19 +0900

    Add files via upload

---
+++ MoviePlayer/MoviePlayer/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  MoviePlayer
+//
+//  Created by 박경춘 on 2023/01/15.
+//
+
+import UIKit
+import AVKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnPlayInternalMovie(_ sender: UIButton) {
+        let filePath:String? = Bundle.main.path(forResource: "FastTyping", ofType: "mp4")
+        let url = NSURL(fileURLWithPath: filePath!)
+        
+        playVideo(url: url)
+    }
+    
+    @IBAction func btnPlayExteranlMovie(_ sender: UIButton) {
+        let url = NSURL(string: "https://dl.dropboxusercontent.com/s/e38auz050w2mvud/Fireworks.mp4")!
+        
+       playVideo(url: url)
+    }
+    
+    private func playVideo(url: NSURL){
+        let playerController = AVPlayerViewController()
+        
+        let player = AVPlayer(url: url as URL)
+        playerController.player = player
+        
+        self.present(playerController, animated: true)
+        {
+            player.play()
+        }
+    }
+}
+
